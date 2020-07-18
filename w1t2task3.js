61e5c0d27cdbb4c5a9e3c0a0370b064cf3fbe74a
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-18 10:37:08 +0700

    mengubah kondisi menjadi type data number

--- w1t2task3.js
+++ w1t2task3.js
@@ -1,7 +1,6 @@
 const printSegitiga = 5;
-const angka = 1;
 
-if (typeof printSegitiga == typeof angka) {
+if (typeof printSegitiga === 'number') {
     for (let i = printSegitiga; i >= 1; i--) {
         let output = "";
         for (let j = i; j >= 1; j--) output += j;
