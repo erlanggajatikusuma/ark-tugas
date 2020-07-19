66b28b0df304b15f6ac08929f622320977418821
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-19 09:46:29 +0700

    menambahkan validasi dan revisi rata2

--- w1t2task2.js
+++ w1t2task2.js
@@ -3,18 +3,31 @@ const bahasaIndonesia = 90;
 const bahasaInggris = 89;
 const ipa = 69;
 
+let UN = [mtk, bahasaIndonesia, bahasaInggris, ipa];
+let num = 0;
 
-let rata2 = (mtk + bahasaIndonesia + bahasaInggris + ipa)/4;
+// Validasi
+if(mtk && bahasaIndonesia && bahasaInggris && ipa != 0) {
+    for (let i = 0; i < UN.length; i++) {
+        num += +UN[i];
+    }
+    // Rata-rata
+    let rata2 = num/4;
+    console.log(rata2);
 
-if (rata2 >= 90) {
-    "A"
-} else if (rata2 >= 80) {
-    "B"
-} else if (rata2 >= 70) {
-    "C"
-} else if (rata2 >= 60) {
-    "D"
+    // Grade
+    if (rata2 >= 90) {
+        "A"
+    } else if (rata2 >= 80) {
+        "B"
+    } else if (rata2 >=70) {
+        "C"
+    } else if (rata2 >= 60) {
+        "D"
+    } else {
+        "E"
+    }
+
 } else {
-    "E"
+    console.log('error')
 }
-
