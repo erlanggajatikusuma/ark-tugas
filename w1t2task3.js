f078e411d54412ca1b6d176339e4dc630a3d5f4c
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-20 10:38:42 +0700

    revisi perulangan

--- w1t2task3.js
+++ w1t2task3.js
@@ -3,10 +3,12 @@ const printSegitiga = 5;
 if (typeof printSegitiga === 'number') {
     for (let i = printSegitiga; i >= 1; i--) {
         let output = "";
-        for (let j = i; j >= 1; j--) output += j;
+        for (let j = 1; j <= i; j++) output += j;
         console.log(output);
     }
 } else {
     console.log("Data harus number");
 }
+
+
 
