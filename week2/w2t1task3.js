624d30e7cef93baed7bd4ad5b9605e15ec8c99e8
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-22 05:46:58 +0700

    Menambahkan sortir data

--- week2/w2t1task3.js
+++ week2/w2t1task3.js
@@ -1,11 +1,12 @@
 const seleksiNilai = (num1, num2, arr) => {
     if(num1 < num2 && arr.length > 5 ) {
-    // sort
-        arr.filter(item => {
+        const data = arr.filter(item => {
             if(num1 < item && item <= num2) {
-                console.log(item);
+                return item;
             }
         });
+        const dataSort = data.sort((a, b) => a-b);
+        console.log(dataSort);
     } else if ( num1 > num2) {
         console.log('Nilai akhir harus lebih besar dari nilai awal');
     } else if (arr.length < 5) {
