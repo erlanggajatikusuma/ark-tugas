540801b00ac8abeec18394582844072d3e8d8652
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-24 09:51:16 +0700

    revisi null

--- week2/w2t2task2.js
+++ week2/w2t2task2.js
@@ -1,6 +1,6 @@
 const getMonth = (callback) => {
     setTimeout(() => {
-        let error =false;
+        let error = false;
         let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         if(!error) {
             callback(null, months);
@@ -11,7 +11,13 @@ const getMonth = (callback) => {
 }
 
 const getData = (err, dataMonths) => {
-    const months = dataMonths.map(month => console.log(month));
+    if(err == null) {
+        const months = dataMonths.map(month => console.log(month));
+    } else {
+        console.log(err);
+    }
+
+
 }
 
 getMonth(getData);
