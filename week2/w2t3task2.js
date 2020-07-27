089d7a9ceaa1463b9fc31371da5e53cd3735db2d
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-27 11:53:24 +0700

    versi 2

--- week2/w2t3task2.js
+++ week2/w2t3task2.js
@@ -1,11 +1,12 @@
 // Divide and sort
 
+// Versi 2
 const divideAndSort = (number) => {
     const numbers = number.toString().split(0);
     const output = [];
   
     for (let i = 0; i < numbers.length-1; i++) {
-      const sorts = numbers[i].split('').sort((a, b) => a-b).join('');
+      const sorts = numbers[i].split('').sort().join('');
   
       output.push(sorts);
     }
@@ -13,4 +14,4 @@ const divideAndSort = (number) => {
   }
   
   
-  divideAndSort(5458402344306574);
+  divideAndSort(5210993201104300);
