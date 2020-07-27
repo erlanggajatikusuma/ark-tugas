603d45738e5d8663af0b757bad62743363cef9b9
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-07-27 11:51:58 +0700

    revisi 2

--- week2/w2t3task2.js
+++ week2/w2t3task2.js
@@ -1,8 +1,16 @@
-const divideAndSort = (number) => {
-    const sorts = [...`${number}`].sort();
-    const numbers = Number(sorts.join(''))
-    
-    console.log(numbers);
-}
+// Divide and sort
 
-divideAndSort(120409430)
+const divideAndSort = (number) => {
+    const numbers = number.toString().split(0);
+    const output = [];
+  
+    for (let i = 0; i < numbers.length-1; i++) {
+      const sorts = numbers[i].split('').sort((a, b) => a-b).join('');
+  
+      output.push(sorts);
+    }
+    return Number(output.join(''));
+  }
+  
+  
+  divideAndSort(5458402344306574);
